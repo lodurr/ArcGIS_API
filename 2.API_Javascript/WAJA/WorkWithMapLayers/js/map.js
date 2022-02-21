1a75252f6861ff40e88f198d9b56afb649bb2d82
Diego Cordero <ing.diegocordero@gmail.com>
2022-02-21 00:07:55 -0400

    Exercise 2
    Exercise 2

--- 2.API_Javascript/WAJA/WorkWithMapLayers/js/map.js
+++ 2.API_Javascript/WAJA/WorkWithMapLayers/js/map.js
@@ -2,18 +2,23 @@ var mapMain;
 
 // @formatter:off
 require([
-        "esri/map",
-
-        "dojo/ready",
-        "dojo/parser",
-        "dojo/on",
-
-        "dijit/layout/BorderContainer",
-        "dijit/layout/ContentPane"],
-    function (Map,
-              ready, parser, on,
-              BorderContainer, ContentPane) {
-// @formatter:on
+    "esri/map",
+    "esri/geometry/Extent",
+    "esri/layers/ArcGISDynamicMapServiceLayer",
+    "esri/layers/FeatureLayer",
+    "esri/dijit/BasemapToggle",
+    "esri/dijit/Scalebar",
+    "esri/dijit/Legend",
+    "dojo/dom",
+    "dojo/ready",
+    "dojo/parser",
+    "dojo/on",
+    "dijit/layout/BorderContainer",
+    "dijit/layout/ContentPane",
+    "dojo/domReady!"],
+    function (Map, Extent, ArcGISDynamicMapServiceLayer, FeatureLayer, BasemapToggle, Scalebar, Legend, dom, ready, parser, on,
+        BorderContainer, ContentPane) {
+        // @formatter:on
 
         // Wait until DOM is ready *and* all outstanding require() calls have been resolved
         ready(function () {
@@ -26,37 +31,65 @@ require([
              * Note: Exact coordinates may vary slightly from snippet/solution
              * Reference: https://developers.arcgis.com/javascript/jssamples/fl_any_projection.html
              */
-
+            var extentInitial = new Extent({
+                "xmin": -14521585.856836917,
+                "ymin": 3563149.775533272,
+                "xmax": -12503648.310108801,
+                "ymax": 5331596.861938639,
+                "spatialReference": { "wkid": 102100, "latestWkid": 3857 }
+            });
 
             // Create the map
             mapMain = new Map("cpCenter", {
-                basemap: "topo",
-                center: [-122.45, 37.75],
-                zoom: 12
+                basemap: "satellite",
+                extent: extentInitial
             });
 
             /*
              * Step: Add the USA map service to the map
              */
+            var lyrUSA = new ArcGISDynamicMapServiceLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer", {
+                opacity: 0.5
+            });
 
 
             /*
              * Step: Add the earthquakes layer to the map
              */
+            var lyrQuakes = new FeatureLayer("https://services.arcgis.com/ue9rwulIoeLEI9bj/ArcGIS/rest/services/Earthquakes/FeatureServer/0");
+            lyrQuakes.setDefinitionExpression("MAGNITUDE  >= 2.0");
 
             /*
             * Step: Revise code to use the addLayers() method
             */
+            mapMain.addLayers([lyrUSA, lyrQuakes]);
 
             /*
              * Step: Add the BaseMapToggle widget to the map
              */
-
+            var toggle = new BasemapToggle({
+                map: mapMain,
+                basemap: "topo"
+            }, "BasemapToggle");
+            toggle.startup();
 
             /*
              * Step: Add a legend once all layers have been added to the map
              */
-            //mapMain.on(); // stub
+
+            var dijitScalebar = new Scalebar({
+                map: mapMain,
+                scalebarUnit: "dual",
+                attachTo: "bottom-left"
+            });
+            mapMain.on("layers-add-result", function() {
+                var dijitLegend = new Legend({
+                    map: mapMain,
+                    arrangement: Legend.ALIGN_RIGHT
+                }, "divLegend");
+                dijitLegend.startup();
+            }); // stub
+            
 
 
         });
